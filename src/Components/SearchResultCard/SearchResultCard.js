6ffde7652c1027a32be06a84855d6e324504952c
Conner Turmon <connerturmon@gmail.com>
2020-02-24 09:08:00 -0600

    Movie id now pulled from pathname. Started building details page

--- src/Components/SearchResultCard/SearchResultCard.js
+++ src/Components/SearchResultCard/SearchResultCard.js
@@ -7,12 +7,7 @@ const SearchResultCard = props => {
         <Link
             className="search-result-card"
             style={{textDecoration: 'none'}}
-            to={{
-                pathname: `/movie/${props.movie.id}`,
-                state: {
-                    movie: props.movie.id
-                }
-            }}
+            to={`/movie/${props.movie.id}`}
         >
             <h1 className="search-result-card__title">{props.movie.title}</h1>
             <img alt="Movie Backdrop" className="search-result-card__bg" src={`https://image.tmdb.org/t/p/w500${props.movie.backdrop_path}`} />
--- src/Pages/MoviePage/MoviePage.js
+++ src/Pages/MoviePage/MoviePage.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import './MoviePage.scss';
 import { TMDB_API_KEY } from '../../constants/keys';
 
 const MoviePage = props => {
-    const [movieDeatils, setMovieDetails] = useState({});
+    const [movieDetails, setMovieDetails] = useState({});
 
+    // Fetch movie details based on the ID
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${props.location.state.movie}?api_key=${TMDB_API_KEY}&language=en-US`)
+        // ID is retrieved from the pathname by removing the
+        // '/movie/' part of the path.
+        // eslint-disable-next-line
+        const movieid = props.location.pathname.substring(7);
+        
+        fetch(`https://api.themoviedb.org/3/movie/${movieid}?api_key=${TMDB_API_KEY}&language=en-US`)
         .then(response => {
             return response.json();
         })
@@ -13,11 +20,18 @@ const MoviePage = props => {
             console.log(providedResults);
             setMovieDetails(providedResults);
         });
-    }, [])
+    }, [props.location.pathname])
 
     return (
-        <div>
-            <h1>{movieDeatils.title}</h1>
+        <div className="movie-page">
+            <div className="movie-page__backdrop-container">
+                <img alt="Movie Backdrop" className="movie-page__backdrop-container__bg" src={`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`} />
+            </div>
+            <img alt="Movie Poster" className="movie-page__poster" src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`} />
+            <div className="movie-page__details">
+                <h1 className="movie-page__details__title">{movieDetails.title}</h1>
+                <p className="movie-page__details__overview">{movieDetails.overview}</p>
+            </div>
         </div>
     );
 }
